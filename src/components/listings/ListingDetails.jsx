8e81a0733ac527f343c7a8a914891821dbe77fc0
MakoDoli <makodolidze@hotmail.com>
2024-09-18 20:43:55 +0400

    fix paddings

--- src/components/listings/ListingDetails.jsx
+++ src/components/listings/ListingDetails.jsx
@@ -12,7 +12,6 @@ import useListingDetails from "../../hooks/useListingDetails";
 import { boldFont, slimFont, mediumFont } from "@/app/fonts/fontWeight";
 import Link from "next/link";
 import Spinner from "../ui/Spinner";
-
 import useDeleteListing from "@/hooks/useDeleteListing";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
@@ -72,57 +71,69 @@ export default function ListingDetails({ id, region }) {
         />
 
         <div>
-          <p className={`${boldFont.className} text-[48px] mt-[30px]`}>
+          <p
+            className={`${boldFont.className} text-[48px] mt-[30px] leading-[57px]`}
+          >
             {price} ₾
           </p>
-          <div className="flex flex-col gap-4 mt-[24px]">
-            <div className="flex gap-2 items-center">
-              <img
-                src="/icons/locpin.png"
-                className="w-[22px] h-[22px]"
-                alt="location"
-              />
-              <p className={`${slimFont.className} text-detailsText text-2xl`}>
-                {city.name}, {address}
-              </p>
-            </div>
-            <div className="flex gap-2 items-center">
-              <img
-                src="/icons/square.png"
-                className="w-[22px] h-[22px]"
-                alt="area"
-              />
-              <p className={`${slimFont.className} text-detailsText text-2xl`}>
-                ფართი {area} მ<sup>2</sup>
-              </p>
-            </div>
-            <div className="flex gap-2 items-center">
-              <img
-                src="/icons/bed.png"
-                className="w-[22px] h-[22px]"
-                alt="bed"
-              />
-              <p className={`${slimFont.className} text-detailsText text-2xl`}>
-                საძინებელი {bedrooms}
-              </p>
-            </div>
-            <div className="flex gap-2 items-center">
-              <img
-                src="/icons/zipcode.png"
-                className="w-[22px] h-[22px]"
-                alt="zipcode"
-              />
-              <p className={`${slimFont.className} text-detailsText text-2xl`}>
-                საფოსტო ინდექსი {zip_code}
-              </p>
+          <div className="flex flex-col  mt-[24px]">
+            <div className="flex flex-col gap-4">
+              <div className="flex gap-2 items-center">
+                <img
+                  src="/icons/locpin.png"
+                  className="w-[16px] h-[19px]"
+                  alt="location"
+                />
+                <p
+                  className={`${slimFont.className} text-detailsText text-[24px] leading-7`}
+                >
+                  {city.name}, {address}
+                </p>
+              </div>
+              <div className="flex gap-2 items-center">
+                <img
+                  src="/icons/square.png"
+                  className="w-[17px] h-[17px]"
+                  alt="area"
+                />
+                <p
+                  className={`${slimFont.className} text-detailsText text-[24px] leading-7`}
+                >
+                  ფართი {area} მ<sup>2</sup>
+                </p>
+              </div>
+              <div className="flex gap-2 items-center">
+                <img
+                  src="/icons/bed.png"
+                  className="w-[22px] h-[22px]"
+                  alt="bed"
+                />
+                <p
+                  className={`${slimFont.className} text-detailsText text-[24px] leading-7`}
+                >
+                  საძინებელი {bedrooms}
+                </p>
+              </div>
+              <div className="flex gap-2 items-center">
+                <img
+                  src="/icons/zipcode.png"
+                  className="w-[18px] h-[21px]"
+                  alt="zipcode"
+                />
+                <p
+                  className={`${slimFont.className} text-detailsText text-[24px] leading-7`}
+                >
+                  საფოსტო ინდექსი {zip_code}
+                </p>
+              </div>
             </div>
             <div
-              className={`${slimFont.className} mt-[40px] text-detailsText text-[16px]`}
+              className={`${slimFont.className} mt-[40px] text-detailsText text-[16px] h-[78px] leading-[26px]`}
             >
               {description}
             </div>
             <div
-              className={`${slimFont.className} text-[14px] text-detailsText px-[20px] py-[24px] mt-[50px] border border-gray-200 rounded-lg`}
+              className={`${slimFont.className} text-[14px] leading-[17px] text-detailsText px-[20px] pt-[24px] pb-[20px] w-[503px] mt-[50px] border border-gray-200 rounded-lg`}
             >
               <div className="flex gap-[14px] items-center">
                 <Image
@@ -133,13 +144,13 @@ export default function ListingDetails({ id, region }) {
                   className="rounded-full"
                 />
                 <div className="flex flex-col gap-1">
-                  <p className="text-[16px] text-iconGray">
+                  <p className="text-[16px] leading-[19px] text-iconGray">
                     {name} {surname}
                   </p>
                   <p>აგენტი</p>
                 </div>
               </div>
-              <div className="flex gap-1 items-center mt-[18px]">
+              <div className="flex gap-1 items-center mt-[16px]">
                 <img src="/icons/email.png" alt="email" className="h-[13px]" />
                 <p>{email}</p>
               </div>
@@ -151,7 +162,7 @@ export default function ListingDetails({ id, region }) {
             <Dialog>
               <DialogTrigger>
                 <div
-                  className={`${mediumFont.className} w-[131px] h-[34px] border border-deleteGray rounded-md text-[12px] text-deleteGray flex justify-center items-center cursor-pointer`}
+                  className={`${mediumFont.className} w-[131px] h-[34px] border border-deleteGray rounded-md text-[12px] text-deleteGray mt-5 flex justify-center items-center cursor-pointer`}
                   onClick={() => setOpen(true)}
                 >
                   ლისტინგის წაშლა
